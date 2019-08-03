50a6ce45807c832f467cb84ab7ea33be219d7e9b
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2019-08-03 23:12:42 +0200

    Began working on API

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,8 +9,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/feedback/create', (req, res) => {
-  res.json(req.body);
+app.post('/feedback/create', async (req, res) => {
+  const {
+    feedback,
+    sender_email: senderEmail
+  } = req.body;
+
+  if (!feedback) {
+    return res.json({
+      error_description: 'No feedback provided.'
+    });
+  }
+
+  const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const senderEmailIsValid = senderEmail && EMAIL_REGEX.test(senderEmail);
+
+  await sendEmail({
+    feedback,
+    senderEmail: senderEmailIsValid ? senderEmail : null
+  });
+
+  if (senderEmail && !senderEmailIsValid) {
+    return res.json({
+      success: true,
+      warning_description: 'Note: the sender e-mail address was excluded from your feedback since it was invalid.'
+    });
+  }
+
+  res.json({
+    success: true
+  });
 });
 
 const PORT = 8080 || process.env.PORT;
